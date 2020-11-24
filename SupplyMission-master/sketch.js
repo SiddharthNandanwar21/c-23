9a45084e673d1e7c244f9b45a7f3620ecc4d0a10
SiddharthNandanwar21 <69305857+SiddharthNandanwar21@users.noreply.github.com>
2020-11-24 17:40:49 +0530

    Add files via upload

---
+++ SupplyMission-master/sketch.js
@@ -0,0 +1,88 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground,s1,s2,s3
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:1, isStatic:true});
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+	 var s1Options = {
+		 isStatic:true
+	 }
+	 var s2Options = {
+		isStatic:true
+	}
+	var s3Options = {
+		isStatic:true
+	}
+	 //bottom
+	 s1 = Bodies.rectangle(width/2,645,100,20,s1Options);
+	 World.add(world,s1);
+	 s2= Bodies.rectangle(width/2-150,595,20,100,s2Options);
+	 World.add(world,s2);
+	 s3= Bodies.rectangle(width/2+150,595,20,100,s3Options);
+	 World.add(world,s3);
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x ;
+  packageSprite.y= packageBody.position.y ;
+
+  //bottom
+  fill("red")
+  rect(s1.position.x,s1.position.y,300,20);
+  rect(s2.position.x,s2.position.y,20,100);
+  rect(s3.position.x,s3.position.y,20,100);
+
+  drawSprites();
+ 
+}
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+    // Look at the hints in the document and understand how to make the package body fall only on press of the Down arrow key.
+Matter.Body.setStatic(packageBody,false)
+
+  }
+}
+
+
+
